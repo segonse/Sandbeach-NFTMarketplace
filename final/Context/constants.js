b1cc41d86a97c7e0eba1b8f1e06f857a12e708e2
segon <2891889980@qq.com>
2024-11-22 19:51:45 +0800

    .

--- final/Context/constants.js
+++ final/Context/constants.js
@@ -1,12 +1,12 @@
-// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// celo mainnet
 
 import NFTMarketplace from "./NFTMarketplace.json";
 import TransferFunds from "./TransferFunds.json";
 
 export const NFTMarketplaceABI = NFTMarketplace.abi;
 export const NFTMarketplaceAddress =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0x72FAD1F64dBb0bBe27EDe19616409237cbDd1262";
 
 export const TransferFundsABI = TransferFunds.abi;
 export const TransferFundsAddress =
-  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  "0x469696BE8bFDc11ad832CE75126565Fc54630345";
--- final/hardhat.config.cjs
+++ final/hardhat.config.cjs
@@ -5,10 +5,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    polygon: {
-      url: process.env.POLYGON_RPC_URL,
+    celo: {
+      url: process.env.CELO_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-      chainId: 137,
+      chainId: 42220,
     },
   },
 };
